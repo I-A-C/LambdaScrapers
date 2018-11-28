64a3d609a3414f48012f72e0a1497cb756c89fcc
jewbmx <42426698+jewbmx@users.noreply.github.com>
2018-11-27 19:36:26 -0600

    Update __init__.py

--- lib/lambdascrapers/sources_notworking/__init__.py
+++ lib/lambdascrapers/sources_notworking/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+# Should remove any other nojoy folders and make this the spot for em all.
+# Also Might wanna change the folder layout yo account for languages(..en,de,..)
 
 
